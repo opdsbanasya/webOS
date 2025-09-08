89b9c625c107dcf53ee12601de76d1096bd1d908
DHARM <opdsbanasya@gmail.com>
2025-09-08 10:40:27 +0530

    setting up frontend project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router";
+import Desktop from "./Components/Desktop";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Browser from "./Components/Browser";
+import Bash from "./Components/Bash";
+import Editor from "./Components/Editor";
+import FileExplorer from "./Components/FileExplorer";
 
 const App = () => {
   return (
-    <div>Collabarative Document platform</div>
-  )
-}
+    <Routes>
+      <Route index element={<Desktop />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/browser" element={<Browser />} />
+      <Route path="/bash" element={<Bash />} />
+      <Route path="/editor" element={<Editor />} />
+      <Route path="/file-explorer" element={<FileExplorer />} />
+    </Routes>
+  );
+};
 
 export default App;
+/**
+ * - /
+- login
+- register
+- terminal
+- file-explorer
+- text-editor
+- browser
+ */
---
+++ frontend/src/Components/Bash.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bash = () => {
+  return (
+    <div>Bash</div>
+  )
+}
+
+export default Bash
